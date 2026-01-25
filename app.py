b5e6ade507eb328937015cd6e031aaee6be38e9a
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2026-01-25 14:54:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/ner' , methods=["GET", "POST"])
 def ner():
